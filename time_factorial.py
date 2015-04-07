d432d1e25b08767d10c147a0d8373fe94ac656c7
Feilong Ma <mafeilong@gmail.com>
2015-04-07 00:48:02 -0400

    try to make travis happy

--- time_factorial.py
+++ time_factorial.py
@@ -3,7 +3,7 @@ import time
 from factorial import factorial_recursive, factorial_reduce, factorial_loop
 
 
-def time_elapsed(func, n, repeat=1000):
+def time_elapsed(func, n, repeat=10000):
     t_start = time.clock()
     for j in xrange(repeat):
         func(n)
@@ -12,13 +12,14 @@ def time_elapsed(func, n, repeat=1000):
 
 
 # for large n's, non-recursive functions only take ~30% of the time
-func_names = ['factorial_recursive', 'factorial_reduce', 'factorial_loop']
+funcs = [('recursive', factorial_recursive),
+         ('reduce', factorial_reduce),
+         ('loop', factorial_loop)]
 for n in [1, 3, 5, 10, 20, 50, 80, 100, 120]:
     times = {}
-    for func_name in func_names:
-        func = eval(func_name)
+    for func_name, func in funcs:
         times[func_name] = time_elapsed(func, n)
     print '%3d, %.2f, %.2f' % (
         n,
-        times['factorial_reduce'] / times['factorial_recursive'],
-        times['factorial_loop'] / times['factorial_recursive'])
+        times['reduce'] / times['recursive'],
+        times['loop'] / times['recursive'])
